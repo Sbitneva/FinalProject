b5c436e10df2500307cebf6106a2e5d4a8b133c6
Sbitneva <masha.sbitneva@gmail.com>
2018-01-07 02:19:12 +0200

    Embedded tomcat servlet project skeleton

---
+++ src/main/java/sbitneva/servlets/ServletDispatcher.java
@@ -0,0 +1,29 @@
+package sbitneva.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/")
+public class ServletDispatcher extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
