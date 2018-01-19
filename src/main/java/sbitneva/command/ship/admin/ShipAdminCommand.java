19d5ee7a50f2ebcee0034e1cb1499c02b4994d36
Sbitneva <masha.sbitneva@gmail.com>
2018-01-19 03:28:37 +0200

    ShipAdmin Command completed.

--- src/main/java/sbitneva/command/ship/admin/ShipAdminCommand.java
+++ src/main/java/sbitneva/command/ship/admin/ShipAdminCommand.java
@@ -1,7 +1,10 @@
 package sbitneva.command.ship.admin;
 
 import org.apache.log4j.Logger;
+import sbitneva.command.CommandsHelper;
 import sbitneva.command.factory.Command;
+import sbitneva.entity.Ship;
+import sbitneva.services.ship.admin.ShipAdminService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +16,18 @@ public class ShipAdminCommand implements Command {
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
         log.debug("execution " + request.getQueryString());
+        ShipAdminService shipAdminService = ShipAdminService.getShipAdminService();
+        try {
+            int userId = Integer.parseInt(request.getSession().getAttribute(CommandsHelper.USER_ID_SESSION_ATTRIBUTE).toString());
+            Ship ship = shipAdminService.getShip(userId);
+            if(ship != null) {
+                request.setAttribute(CommandsHelper.SHIP, ship);
+                request.getRequestDispatcher(CommandsHelper.SHIP_INFO_PAGE).forward(request, response);
+            }
+
+        } catch (Exception e){
+
+        }
 
     }
 }
