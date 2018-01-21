127a3d1010c2429ae9aa4c5e9813151eb22c3d7d
Sbitneva <masha.sbitneva@gmail.com>
2018-01-22 01:46:18 +0200

    Small issues

--- src/main/java/sbitneva/command/client/AddTicketToCartCommand.java
+++ src/main/java/sbitneva/command/client/AddTicketToCartCommand.java
@@ -1,5 +1,6 @@
 package sbitneva.command.client;
 
+import org.apache.log4j.Logger;
 import sbitneva.command.factory.Command;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AddTicketToCartCommand implements Command {
+    private static Logger log = Logger.getLogger(AddTicketToCartCommand.class.getName());
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
